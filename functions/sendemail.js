9c6ef53a4e1efe825df872740be9228b3f32bd1b
andrefbcorreia <andrefbcorreia@gmail.com>
2020-12-23 11:54:39 +0000

    update netlifly function

--- functions/sendemail.js
+++ functions/sendemail.js
@@ -1,15 +1,31 @@
 const nodemailer = require('nodemailer');
 
 exports.handler = (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  
+  console.log(data);
+  
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.zoho.eu',
+    port: 465,
+    secure: true,
+  });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      result : 'success',
-    }),
-  };
-
-  // const data = JSON.parse(event.body);
+  transporter.sendMail({
+    from: 'andrecorreia@zohomail.eu',
+    to: 'andrefbcorreia@gmail.com',
+    subject: 'Contact from andrefbcorreia.github.io page',
+    html: `
+      <h3>Email from me eheh<h3>
+      <p>Data message<p>
+    `,
+  }, (err) => {
+    if (err) {
+      console.log("error", err);
+    } else {
+      console.log("All ok");
+    }
+  });
 
   // const transporter = nodemailer.createTransport({
   //   host: 'smtp.zoho.eu',
