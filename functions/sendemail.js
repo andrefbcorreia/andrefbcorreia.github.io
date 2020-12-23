f64b44bde8983a58aeb39af6eae8cf3045a29a43
andrefbcorreia <andrefbcorreia@gmail.com>
2020-12-23 11:44:26 +0000

    Add send email test function

---
+++ functions/sendemail.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+
+exports.handler = (event, context, callback) => {
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      'result' : 'success',
+    }),
+  };
+
+  // const data = JSON.parse(event.body);
+
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.zoho.eu',
+  //   port: 465,
+  //   secure: true,
+  // });
+
+  // transporter.sendMail({
+  //   from: 'andrecorreia@zohomail.eu',
+  //   to: 'andrefbcorreia@gmail.com',
+  //   subject: 'Contact from andrefbcorreia.github.io page',
+  //   html: `
+  //     <h3>Email from ${data.name} ${data.email}<h3>
+  //     <p>${data.message}<p>
+  //   `,
+  // }, (err) => {
+  //   if (err) {
+  //     callback(err);
+  //   } else {
+  //     callback(null, {
+  //       statusCode: 200,
+  //       body: JSON.stringify({
+  //         'result' : 'success',
+  //       }),
+  //     });
+  //   }
+  // });
+};
