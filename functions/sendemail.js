287de26fb09a2afe000c92290a1f0db1f398f53d
andrefbcorreia <andrefbcorreia@gmail.com>
2020-12-23 15:59:38 +0000

    update netlifly function

--- functions/sendemail.js
+++ functions/sendemail.js
@@ -4,42 +4,35 @@ exports.handler =  async (event, context, callback) => {
   //const data = JSON.parse(event.body);
   
   //console.log(data);
-  
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      'result' : 'success',
-    }),
-  };
-  
-  // const transporter = nodemailer.createTransport({
-  //   host: 'smtp.gmail.com',
-  //   port: 587,
-  //   secure: true,
-  // });
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: true,
+  });
 
 
-  // transporter.sendMail({
-  //   from: 'andrecorreia@zohomail.eu',
-  //   to: 'andrefbcorreia@gmail.com',
-  //   subject: 'Contact from andrefbcorreia.github.io page',
-  //   html: `
-  //     <h3>Email from me eheh<h3>
-  //     <p>Data message<p>
-  //   `,
-  // }, (err) => {
-  //   if (err) {
-  //     return {
-  //       statusCode: 400,
-  //       body: `Error: ${err}`
-  //     };
-  //   } else {
-  //     return {
-  //       statusCode: 200,
-  //       body: "All ok",
-  //     };
-  //   }
-  // });
+  transporter.sendMail({
+    from: 'andrecorreia@zohomail.eu',
+    to: 'andrefbcorreia@gmail.com',
+    subject: 'Contact from andrefbcorreia.github.io page',
+    html: `
+      <h3>Email from me eheh<h3>
+      <p>Data message<p>
+    `,
+  }, (err) => {
+    if (err) {
+      return {
+        statusCode: 400,
+        body: `Error: ${err}`
+      };
+    } else {
+      return {
+        statusCode: 200,
+        body: "All ok",
+      };
+    }
+  });
 
   // const transporter = nodemailer.createTransport({
   //   host: 'smtp.zoho.eu',
