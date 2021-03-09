2c70cf32b8437352c2a6431ef1ab36183c6ccf2a
André Correia <AndreCorreia@Andres-MacBook-Pro-2.local>
2021-03-09 22:55:57 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 
 import About from './components/about/About';
-import Experience from './components/experience/Experience';
 import Skills from './components/skills/Skills';
+
+import image from './assets/images/andre.jpeg';
 
 import './App.css';
 
 const App = () => (
-  <div className="wrapper">
-    <div className="app_container">
-      <div className="app_content">
-        <div className="a1">
-          <About />
-          <div className="app_child">
-            <Experience />
-            <Skills />
-          </div>
+  <div className="container">
+    <div className="content">
+      <img className="me" src={image} alt=""/>
+      <div className="bio">
+        <div className="title">
+          <div className="profile-name">André Correia</div>
+          <div className="profile-job">Software Engineer</div>
         </div>
+        <About />
+        <Skills />
       </div>
+    </div>
+    <div className="line" />
   </div>
-  </div>
-);
+)
+
 
 export default App;
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,28 +4,17 @@ import './About.css';
 
 const About = () => (
   <div className="about_container">
-    <div className="about_title">
-      <span>About </span> 
-      <span className="about_title_me">Me</span>
-    </div>
     <div className="about_description">
       <p>
-        My name is <span>André Correia</span>, I was born in Switzerland, lived most of my life in the shiny south of Portugal 
-        and I am a <span>Software Engineer</span> currently working in Lisbon.
-      </p>
-      <p>
-        In high school, I started to enjoy programming and that's why I took <span>Computer Science</span> in 
+        Born in Switzerland, lived most of my life in the shiny south of Portugal 
+        and currently working in Lisbon. Took <span>Computer Science</span> in 
         <span> FCT NOVA Lisboa</span>, where I develop my knowledge base and passion for building beautiful and 
         functional web applications.
       </p>
       <p>
         I've been mainly working as a <span>full stack</span> developer with technologies such as <span>React JS</span>, 
         <span> Node JS</span> and <span>Ruby on Rails</span> in projects that involve a large codebase, 
-        teamwork, unit tests and code reviews.
-      </p>
-      <p>
-        Besides that, I love to travel, going to the beach, gym, hanging out with friends, watch series,
-        play football and computer games.
+        unit tests and code reviews.
       </p>
     </div>
   </div>
